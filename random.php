763fb70cd81d6a7d377e295e514d85340b622af9
Pablo Merino <pablo.perso1995@gmail.com>
2011-07-13 22:12:10 +0200

    fixed a margin issue

--- random.php
+++ random.php
@@ -131,6 +131,7 @@ echo("</div>");
 <!-- PHP END -->
 
 <a href="http://github.com/you"><img style="position: fixed; top: 0; left: 0; border: 0;" src="https://gs1.wac.edgecastcdn.net/80460E/assets/img/5d21241b64dc708fcbb701f68f72f41e9f1fadd6/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67" alt="Fork me on GitHub"></a>
+<br>
 
 </body>
 </html>
