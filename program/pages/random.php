21fe0c04f5bfed3a12112e74bfe29102ddb29fe7
Pablo <pablo.perso1995@gmail.com>
2011-07-31 00:33:48 +0200

    Added livefyre on random post

--- program/pages/random.php
+++ program/pages/random.php
@@ -12,8 +12,13 @@ $content .= "<h2>".$tagValue["title"]."</h2>\n";
 $content .= "<h3>Posted by ".$tagValue["author"]."</h3>\n";
 $content .= "<h3>Posted on ".$tagValue["date"]."</h3>";
 $content .= "<p>".$tagValue["body"]."</p>\n";
-$content .= "</div>";
-
-
+if(!$bloggy_config['livefyre_id'] == "") {
+$content .= "<br><script type='text/javascript' src='http://livefyre.com/wjs/javascripts/livefyre.js'></script><script type='text/javascript'>var fyre = LF({site_id: ".$bloggy_config['livefyre_id'].",version: '1.0'});</script>";
+$content .= "<style>#livefyre{min-height:0px;}</style></div>";
+}
 include('./program/lib/constants.php');
+} else {
+include('./program/pages/404.php');
+}
+
 ?>
