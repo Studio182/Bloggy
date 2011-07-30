619d7c6ffbc75e41bfb92d4e07ee59b56eafd437
Pablo <pablo.perso1995@gmail.com>
2011-07-31 00:28:18 +0200

    Fixed a little bug in viewpost

--- program/pages/viewpost.php
+++ program/pages/viewpost.php
@@ -12,7 +12,6 @@ $content .= "<div id=\"box\">\n";
 $content .= "<center><h1>".$tagValue["date"]."</h1></center>\n";
 $content .= "<h2>".$tagValue["title"]."</h2>\n";
 $content .= "<h3>Posted by ".$tagValue["author"]."</h3>\n";
-$content .= "<h3>Posted on ".$tagValue["date"]."</h3>";
 $content .= "<p>".$tagValue["body"]."</p>\n";
 if(!$bloggy_config['livefyre_id'] == "") {
 $content .= "<br><script type='text/javascript' src='http://livefyre.com/wjs/javascripts/livefyre.js'></script><script type='text/javascript'>var fyre = LF({site_id: ".$bloggy_config['livefyre_id'].",version: '1.0'});</script>";
