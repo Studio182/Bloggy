fdadd11bfbc00c4c4765a9ed6498bffbe13e7220
Pablo <pablo.perso1995@gmail.com>
2011-08-09 14:09:45 +0200

    Added read more

--- program/pages/home.php
+++ program/pages/home.php
@@ -29,7 +29,7 @@ rsort($dirlist);
 
 foreach ($dirlist as $file) {
 
-$file_no_ext = str_replace('.post','',$file);
+	$file_no_ext = str_replace('.post','',$file);
 
 	$tagValue = array();
 	$tagValue = parse('./posts/'.$file);
@@ -37,7 +37,12 @@ $file_no_ext = str_replace('.post','',$file);
 	$content .="<h2><a href=\"?/viewpost/".$file_no_ext."/\">".$tagValue["title"]."</a></h2>\n";
 	$content .= "<h1>".$tagValue["date"]."</h1>\n";
 	$content .= "<h3>Posted by ".$tagValue["author"]."</h3>\n";
-	$content .= "<p>".$tagValue["body"]."</p>\n";
+	if(strlen($tagValue["body"]) >= 250) {
+		$content .= "<p>".substr($tagValue["body"], 0, 250)."... <a href=\"?/viewpost/".$file_no_ext."/\">Read more</a></p>\n";
+	} else {
+		$content .= "<p>".$tagValue["body"]."</p>\n";
+
+	}
 	if($bloggy_config['skin'] == "iphone") {
 		if('./posts/'.$file == './posts/post1.post'){
 			$content .= "</div>";
